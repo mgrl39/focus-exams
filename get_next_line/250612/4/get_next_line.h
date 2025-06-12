76f174384d8f7dde3cdb5e76b0a8526a979fc422
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-06-12 22:41:14 +0200

    add gnl 4

---
+++ get_next_line/250612/4/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 22:40:22 by meghribe          #+#    #+#             */
+/*   Updated: 2025/06/12 22:41:04 by meghribe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdio.h>
+char	*get_next_line(int fd);
+
+# endif
